8171c925bede9e8e9985c99f27f82816d68a789d
Sally Brogan <sallybrogan@Sallys-Air.home>
2017-07-15 14:59:14 +0100

    Add TopNav component

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import TopNav from './components/TopNav/TopNav';
+
+var app = function () {
+
+return (
+
+	<TopNav />)
+}
---
+++ src/components/TopNav/TopNav.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+var TopNav = function () {
+
+return (
+<div>
+Hello from the app
+</div>
+	)
+}
+
+export default TopNav;
